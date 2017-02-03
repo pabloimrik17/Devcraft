edea705f946742e4efbc0e4f5b6e04807bc9a74e
Pablo <pabloimrik17@gmail.com>
2017-02-03 23:30:44 +0100

    Chunk mesh update

--- Assets/Scripts/Chunk.cs
+++ Assets/Scripts/Chunk.cs
@@ -23,9 +23,14 @@ public class Chunk : MonoBehaviour {
 	private Vector2 rockTop = new Vector2(7, 8);
 	private Vector2 rockSide = new Vector2(0, 10);
 
+
 	// Use this for initialization
 	void Start () {
-		
+        mesh = GetComponent<MeshFilter>().mesh;
+        chunkCollider = GetComponent<MeshCollider>();
+
+        CubeTop(0, 0, 0, (byte)TextureType.rock.GetHashCode());
+        UpdateMesh();
 	}
 	
 	// Update is called once per frame
@@ -38,9 +43,36 @@ public class Chunk : MonoBehaviour {
 		newVertices.Add (new Vector3 (x+1, y, z + 1));
 		newVertices.Add (new Vector3 (x+ 1, y, z));
 		newVertices.Add (new Vector3 (x, y, z));
-	}
+
+        newTriangles.Add(faceCount * 4);
+        newTriangles.Add(faceCount * 4 + 1);
+        newTriangles.Add(faceCount * 4 + 2);
+        newTriangles.Add(faceCount * 4);
+        newTriangles.Add(faceCount * 4 + 2);
+        newTriangles.Add(faceCount * 4 + 3);
+
+        Vector2 texturePosition = rockTop;
+
+        newUV.Add(new Vector2(textureWidth * texturePosition.x + textureWidth, textureWidth * texturePosition.y));
+        newUV.Add(new Vector2(textureWidth * texturePosition.x, textureWidth * texturePosition.y + textureWidth));
+        newUV.Add(new Vector2(textureWidth * texturePosition.x, textureWidth * texturePosition.y + textureWidth));
+        newUV.Add(new Vector2(textureWidth * texturePosition.x + textureWidth, textureWidth * texturePosition.y));
+    }
 
 	void UpdateMesh() {
+        mesh.Clear();
 
+        mesh.vertices = newVertices.ToArray();
+        mesh.uv = newUV.ToArray();
+        mesh.triangles = newTriangles.ToArray();
+        mesh.RecalculateNormals();
+
+        chunkCollider.sharedMesh = null;
+        chunkCollider.sharedMesh = mesh;
+
+        newVertices.Clear();
+        newUV.Clear();
+        newTriangles.Clear();
+        faceCount = 0;
 	}
 }
