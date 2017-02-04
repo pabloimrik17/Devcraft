f9650d9068d542bd16da1183ea11d44cc0d396c7
Pablo <pabloimrik17@gmail.com>
2017-02-04 13:37:43 +0100

    Plain world with chunks generation.

--- Assets/Scripts/Chunk.cs
+++ Assets/Scripts/Chunk.cs
@@ -13,10 +13,13 @@ public class Chunk : MonoBehaviour {
 
 	private Mesh mesh;
 	private MeshCollider chunkCollider;
-
 	private float textureWidth = 0.083f;
-
 	private int faceCount;
+
+    private World world;
+    [SerializeField] GameObject WorldGO;
+    [SerializeField] int chunkSize = 16;
+    
 
 	private Vector2 grassTop = new Vector2(1, 11);
 	private Vector2 grassSide = new Vector2(0, 10);
@@ -26,40 +29,46 @@ public class Chunk : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
+        world = WorldGO.GetComponent<World>() as World;
+
         mesh = GetComponent<MeshFilter>().mesh;
         chunkCollider = GetComponent<MeshCollider>();
 
-        CubeTop(0, 0, 0, (byte)TextureType.rock.GetHashCode());
-        UpdateMesh();
-	}
+        GenerateMesh();
+    }
 	
 	// Update is called once per frame
 	void Update () {
 		
 	}
 
-	private void CubeTop(int x, int y, int z, byte block) {
-		newVertices.Add (new Vector3 (x, y, z + 1));
-		newVertices.Add (new Vector3 (x+1, y, z + 1));
-		newVertices.Add (new Vector3 (x+ 1, y, z));
-		newVertices.Add (new Vector3 (x, y, z));
+    private void GenerateMesh() {
+        for (int x = 0; x < chunkSize; x++) {
+            for (int y = 0; y < chunkSize; y++) {
+                for (int z = 0; z < chunkSize; z++) {
+                    if(world.Block(x, y, z) != (byte) TextureType.air.GetHashCode()) {
+                        if(world.Block(x, y+1, z) == (byte)TextureType.air.GetHashCode()) {
+                            CubeTop(x, y, z, world.Block(x, y, z));
+                        } else if (world.Block(x, y - 1, z) == (byte)TextureType.air.GetHashCode()) {
+                            CubeBottom(x, y, z, world.Block(x, y, z));
+                        } else if (world.Block(x +1, y, z) == (byte)TextureType.air.GetHashCode()) {
+                            CubeEast(x, y, z, world.Block(x, y, z));
+                        } else if (world.Block(x - 1, y, z) == (byte)TextureType.air.GetHashCode()) {
+                            CubeWest(x, y, z, world.Block(x, y, z));
+                        } else if (world.Block(x, y, z) == (byte)TextureType.air.GetHashCode()) {
+                            CubeNorth(x, y, z, world.Block(x, y, z));
+                        } else if (world.Block(x, y, z-1) == (byte)TextureType.air.GetHashCode()) {
+                            CubeSouth(x, y, z, world.Block(x, y, z));
+                        }
+                    }
+                }
+            }
+        }
 
-        newTriangles.Add(faceCount * 4);
-        newTriangles.Add(faceCount * 4 + 1);
-        newTriangles.Add(faceCount * 4 + 2);
-        newTriangles.Add(faceCount * 4);
-        newTriangles.Add(faceCount * 4 + 2);
-        newTriangles.Add(faceCount * 4 + 3);
-
-        Vector2 texturePosition = rockTop;
-
-        newUV.Add(new Vector2(textureWidth * texturePosition.x + textureWidth, textureWidth * texturePosition.y));
-        newUV.Add(new Vector2(textureWidth * texturePosition.x, textureWidth * texturePosition.y + textureWidth));
-        newUV.Add(new Vector2(textureWidth * texturePosition.x, textureWidth * texturePosition.y + textureWidth));
-        newUV.Add(new Vector2(textureWidth * texturePosition.x + textureWidth, textureWidth * texturePosition.y));
+        UpdateMesh();
     }
 
-	void UpdateMesh() {
+    void UpdateMesh() {
         mesh.Clear();
 
         mesh.vertices = newVertices.ToArray();
@@ -74,5 +83,100 @@ public class Chunk : MonoBehaviour {
         newUV.Clear();
         newTriangles.Clear();
         faceCount = 0;
-	}
+    }
+
+    private void CubeTop(int x, int y, int z, byte block) {
+		newVertices.Add (new Vector3 (x, y, z + 1));
+		newVertices.Add (new Vector3 (x+1, y, z + 1));
+		newVertices.Add (new Vector3 (x+ 1, y, z));
+		newVertices.Add (new Vector3 (x, y, z));
+
+        Vector2 texturePos = rockTop;
+
+        Cube(texturePos);
+
+    }
+
+    private void CubeNorth(int x, int y, int z, byte block)
+    {
+        newVertices.Add(new Vector3(x+1 , y-1, z+1));
+        newVertices.Add(new Vector3(x + 1, y, z+1));
+        newVertices.Add(new Vector3(x, y, z+1));
+        newVertices.Add(new Vector3(x, y-1, z+1));
+
+        Vector2 texturePos = rockTop;
+
+        Cube(texturePos);
+
+    }
+
+    private void CubeEast(int x, int y, int z, byte block)
+    {
+        newVertices.Add(new Vector3(x+1, y-1, z));
+        newVertices.Add(new Vector3(x + 1, y, z));
+        newVertices.Add(new Vector3(x + 1, y, z+1));
+        newVertices.Add(new Vector3(x+1, y-1, z+1));
+
+        Vector2 texturePos = rockTop;
+
+        Cube(texturePos);
+
+    }
+
+    private void CubeSouth(int x, int y, int z, byte block)
+    {
+        newVertices.Add(new Vector3(x, y-1, z));
+        newVertices.Add(new Vector3(x, y, z));
+        newVertices.Add(new Vector3(x + 1, y, z));
+        newVertices.Add(new Vector3(x+1, y-1, z));
+
+        Vector2 texturePos = rockTop;
+
+        Cube(texturePos);
+
+    }
+
+    private void CubeWest(int x, int y, int z, byte block)
+    {
+        newVertices.Add(new Vector3(x, y-1, z + 1));
+        newVertices.Add(new Vector3(x, y, z + 1));
+        newVertices.Add(new Vector3(x, y, z));
+        newVertices.Add(new Vector3(x, y-1, z));
+
+        Vector2 texturePos = rockTop;
+
+        Cube(texturePos);
+
+    }
+
+    private void CubeBottom(int x, int y, int z, byte block)
+    {
+        newVertices.Add(new Vector3(x, y-1, z));
+        newVertices.Add(new Vector3(x + 1, y-1, z));
+        newVertices.Add(new Vector3(x + 1, y-1, z+1));
+        newVertices.Add(new Vector3(x, y-1, z+1));
+
+        Vector2 texturePos = rockTop;
+
+        Cube(texturePos);
+
+    }
+
+    private void Cube(Vector2 texturePos) {
+        newTriangles.Add(faceCount * 4);
+        newTriangles.Add(faceCount * 4 + 1);
+        newTriangles.Add(faceCount * 4 + 2);
+        newTriangles.Add(faceCount * 4);
+        newTriangles.Add(faceCount * 4 + 2);
+        newTriangles.Add(faceCount * 4 + 3);
+
+        newUV.Add(new Vector2(textureWidth * texturePos.x + textureWidth, textureWidth * texturePos.y));
+        newUV.Add(new Vector2(textureWidth * texturePos.x +textureWidth, textureWidth * texturePos.y + textureWidth));
+        newUV.Add(new Vector2(textureWidth * texturePos.x, textureWidth * texturePos.y + textureWidth));
+        newUV.Add(new Vector2(textureWidth * texturePos.x, textureWidth * texturePos.y));
+
+        faceCount++;
+    }
+
+	
 }
---
+++ Assets/Scripts/World.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class World : MonoBehaviour {
+    [SerializeField]
+    private byte[,,] worldData;
+    [SerializeField]
+    int worldX = 16;
+    [SerializeField]
+    int worldY = 16;
+    [SerializeField]
+    int worldZ = 16;
+
+    // Use this for initialization
+    void Start () {
+        worldData = new byte[worldX, worldY, worldZ];
+
+        for (int x = 0; x < worldX; x++) {
+            for (int y = 0; y < worldY; y++) {
+                for (int z = 0; z < worldZ; z++) {
+                    if (y <= 8) {
+                        worldData[x, y, z] = (byte)TextureType.rock.GetHashCode();
+                    }
+                }
+            }
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public byte Block(int x, int y, int z) {
+        if(x >= worldX || x < 0 || y >= worldY || y < 0 || z >= worldZ || z < 0) {
+            return (byte)TextureType.rock.GetHashCode();
+        } else {
+            return worldData[x, y, z];
+        }
+    }
+}
